9373317e78d8b8094bd79b07f44138c360e12ad1
Rasith Upamal <rasithupamal@gmail.com>
2022-09-07 16:33:34 +0530

    app folder restructuring.

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
+import {create, start} from "./server/config/app";
 
-// defining the Express app
-const app = express();
-
-// adding Helmet to enhance your Rest API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
-// defining an endpoint to return all ads
-app.get('/', (req, res) => {
-    res.send("Hello this is blog api.");
-});
-
-// starting the server
-app.listen(3001, () => {
-    console.log('listening on port 3001');
-});
+create();
+start();
